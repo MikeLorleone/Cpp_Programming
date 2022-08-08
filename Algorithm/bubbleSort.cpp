20e8a0509e473da9f7e538958996b4df48361692
Ethan <2283297079@qq.com>
2022-08-08 10:06:51 +0800

    为冒泡排序添加了注释

--- Algorithm/bubbleSort.cpp
+++ Algorithm/bubbleSort.cpp
@@ -2,8 +2,11 @@
  *这是一个带标志的冒泡排序算法
  **************************/
 #include<iostream>
+
+//单趟冒泡函数
 bool bubble (int A [], int n) {
-    bool sorted = true;
+    //设置标志位，若序列中没有逆序对，则说明序列已经有序
+    bool sorted = true; 
     for ( int i = 1; i < n; i++ ){
         if ( A[i - 1] > A[i] ) {
             sorted = false;
@@ -19,6 +22,7 @@ int main () {
      int n = 10;
      int m = n;
     int A [n] = { 9, 7, 5, 3, 4, 2, 8, 0, 6, 1};
+    //设置循环，每循环一次，未排序数组长度减一
     while (!bubble(A, n--));
     for ( int i = 0; i < m; i++ )
         std::cout << A[i] << " ";
